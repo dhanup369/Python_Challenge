564ed4d13ed497c0c3d5b461e922dc413447a89e
Dhanup369 <dhanup369@gmail.com>
2018-06-24 12:23:07 -0700

    Final

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,27 +1,33 @@
 import csv
+#Reading from the CSV file
 with open ('budget_data.csv','r')as file:
      reader=csv.reader(file)
-     csv_header=next(reader)
+     csv_header=next(reader) #CSV file has header in the first row.
+     
+     #Initializing all the variables
      month_count=0
      total=0
      profit=[]
      diff=0
      month=[]
      
+     
      for row in reader:
-         month_count=month_count+1
-         total=total+int(row[1])
-         profit.append(int(row[1]))
-         month.append(row[0])
+         month_count=month_count+1    #Counting the number of rows for counting the number of months
+         total=total+int(row[1])      #Finding the total profits
+         profit.append(int(row[1]))   #Converting the profit column into a list 
+         month.append(row[0])         #Converting the month column into a list
          
      for i in range(len(profit)-1):
-         diff=diff+(profit[i+1]-profit[i])   
-     average_diff=diff/(len(profit)-1)
-     max=max(profit)
-     x=profit.index(max)
-     min=min(profit)
-     y=profit.index(min)
+         diff=diff+(profit[i+1]-profit[i])   #Calculating the change of profit between months over the entire period
+     average_diff=diff/(len(profit)-1)       #Calculating the average change of profit
+     max=max(profit)                         #Finding the maximum value of Profit
+     x=profit.index(max)                     #Finding the index of maximum Profit
+     min=min(profit)                         #Finding the minimum value of Profit
+     y=profit.index(min)                     #Finding the index of minimum Profit
      
+
+     #Printing the analysis to the Terminal 
      print("Financial Analysis: \n......................................")
      print("Total Months: ",month_count)
      print("Total: $"+str(total))
@@ -29,6 +35,8 @@ with open ('budget_data.csv','r')as file:
      print("Greatest increase in profits: ",month[x],"($"+str(max)+")")
      print("Greatest decrease in profits: ",month[y],"($"+str(min)+")")
      
+
+     #Writing the results into a text file to export.
      with open("outputfile.txt",'w') as op:
          op.write("Financial Analysis:\n")
          op.write("...........................\n")
