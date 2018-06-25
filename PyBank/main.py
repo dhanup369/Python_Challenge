6a7452d84ecab688b18e89ef5628354279bd04d4
Dhanup369 <dhanup369@gmail.com>
2018-06-25 16:17:34 -0700

    modified

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,9 +8,9 @@ with open ('budget_data.csv','r')as file:
      month_count=0
      total=0
      profit=[]
-     diff=0
+     aver=0
      month=[]
-     
+     change=[]
      
      for row in reader:
          month_count=month_count+1    #Counting the number of rows for counting the number of months
@@ -19,29 +19,30 @@ with open ('budget_data.csv','r')as file:
          month.append(row[0])         #Converting the month column into a list
          
      for i in range(len(profit)-1):
-         diff=diff+(profit[i+1]-profit[i])   #Calculating the change of profit between months over the entire period
-     average_diff=diff/(len(profit)-1)       #Calculating the average change of profit
-     max=max(profit)                         #Finding the maximum value of Profit
-     x=profit.index(max)                     #Finding the index of maximum Profit
-     min=min(profit)                         #Finding the minimum value of Profit
-     y=profit.index(min)                     #Finding the index of minimum Profit
+         diff=(profit[i+1]-profit[i])   #Calculating the change of profit between months over the entire period
+         aver=aver+diff                 #Recursive addition of the differences to find the average  
+         change.append(diff)            #Adding the differnces in a list 
+     average_diff=aver/(len(profit)-1)       #Calculating the average change of profit
+     maxi=max(change)                         #Finding the maximum value of the difference
+     x=change.index(maxi)                     #Finding the index of maximum difference 
+     mini=min(change)                         #Finding the minimum value of difference
+     y=change.index(mini)                     #Finding the index of minimum difference
      
-
+     
      #Printing the analysis to the Terminal 
      print("Financial Analysis: \n......................................")
      print("Total Months: ",month_count)
      print("Total: $"+str(total))
      print("Average Change: $"+str(average_diff))
-     print("Greatest increase in profits: ",month[x],"($"+str(max)+")")
-     print("Greatest decrease in profits: ",month[y],"($"+str(min)+")")
+     print("Greatest increase in profits: ",month[x+1],"($"+str(maxi)+")")
+     print("Greatest decrease in profits: ",month[y+1],"($"+str(mini)+")")
      
 
      #Writing the results into a text file to export.
      with open("outputfile.txt",'w') as op:
-         op.write("Financial Analysis:\n")
-         op.write("...........................\n")
+         op.write("Financial Analysis:\n..............................\n")
          op.write("Total Months: "+str(month_count)+"\n")
          op.write("Total: $"+str(total)+"\n")
          op.write("Average Change: $"+str(average_diff)+"\n")
-         op.write("Greatest increase in profits: "+str(month[x])+"($"+str(max)+")"+"\n")
-         op.write("Greatest decrease in profits: "+str(month[y])+"($"+str(min)+")"+"\n")
+         op.write("Greatest increase in profits: "+str(month[x+1])+"($"+str(maxi)+")"+"\n")
+         op.write("Greatest decrease in profits: "+str(month[y+1])+"($"+str(mini)+")"+"\n")
