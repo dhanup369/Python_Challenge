c7ec8fd4b391f29e45b8d43a02c144aa692700a1
Dhanup369 <dhanup369@gmail.com>
2018-06-25 14:50:49 -0700

    Final code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,28 +1,36 @@
-
+#Importing the necessary modules
 import csv
 import math
+
+#Initialising the variables
 voter_id=[]
 candidate=[]
 votes=0
 k=[]
 summary={}
+
+#Reading the csv file 
 with open('election_data.csv', mode='r') as infile:
     reader = csv.reader(infile)
-    csv_header=next(reader)
+    csv_header=next(reader)             #CSV file has header in the first row
     for row in reader:
-        votes=votes+1
-        voter_id.append(int(row[0]))
-        candidate.append(row[2])
-    for i in set(candidate):
-        k.append(candidate.count(i))
-    summary=dict(zip(set(candidate),k))
-    sorted_summary=sorted(summary,key=summary.get,reverse=True)
+        votes=votes+1                   #Counting the rows to counts the total number of votes
+        voter_id.append(int(row[0]))    #Converting Voters_id column into a list 
+        candidate.append(row[2])        #Converting candidate column into a list
+    for i in set(candidate):            #Finding the unique candidate names
+        k.append(candidate.count(i))    #Finding the uniques candidates's number of accourance in the candidate list
+    summary=dict(zip(set(candidate),k)) #Creating a Summary dictionary to hold unique candidate names and their number of accourance
+    sorted_summary=sorted(summary,key=summary.get,reverse=True) #Sorting the summary dictionary in decending order
     
+
+
+    #Printing the results to the terminal
     print("Election Results \n.........................\nTotal Vote : "+str(votes)+"\n.........................")
     for i in sorted_summary:
         print(i+":"+str(round((summary[i]/votes)*100,2))+"%"+"("+str(summary[i])+")")
     print("..........................\nWinner: "+sorted_summary[0]+"\n...........................")
     
+    #Writing the results into a text file to export.
     with open("outputfile.txt",'w') as op:
         op.write("Election Results \n.........................\nTotal Vote : "+str(votes)+"\n.........................\n")
         for i in sorted_summary:
